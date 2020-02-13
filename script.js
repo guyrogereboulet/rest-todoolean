7650ca23ebaa8c058e6c1b47dc53022e2de23d9f
Guy Roger Eboulet <eboulet87@gmail.com>
2020-02-13 13:48:45 +0100

    Commit iniziale

---
+++ script.js
@@ -0,0 +1,20 @@
+// Todoolean
+//
+// Creiamo una app che permette di inserire e cancellare dei todos in una lista utilizzando la API boolean per fare operazioni CRUD.
+// BONUS
+// Aggiungiamo update del todo
+// CONSIGLI
+// Provate prima tutte le chiamate con POSTMAN come fatto assieme e salvatele, in modo da poterle usare come riferimento.
+// Cerchiamo di fare tutte le operazioni con delle funzioni riutilizzabili.
+// La grafica non è indispensabile, create un css base solo per poter interagire con i todos
+//
+// Guy Roger Eboulet - http://157.230.17.132:3015/todos
+
+$(document).ready(function(){
+  $.ajax({
+
+  });
+
+
+
+});
